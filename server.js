1a4d0c666f781aaad0076bdbd95dc61523e40caf
Sivakumar <ssiva.victory@gmail.com>
2016-10-04 00:08:57 +0800

    Fix server bind to port error  when run on heroku

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express'),
 app = express();
 app.use(express.static('www'));
 app.set('port', process.env.port || 5000);
-app.listen(app.get('port'), function () {
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+})
+.listen(app.get('port'), function () {
     console.log('express server listening on port ' + app.get('port'));
 });
